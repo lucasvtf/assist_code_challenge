e4c2061094eeee34774b69e167ddc88ef67e3be2
Lucas Victor <lucassvtf@gmail.com>
2024-08-23 12:13:44 -0300

    feat: add login function.

--- backend/src/services/UserService.ts
+++ backend/src/services/UserService.ts
@@ -1,9 +1,9 @@
 import bcrypt from 'bcrypt';
-import IUser from '../interfaces/IUser';
-import UserModel from '../models/UserModel';
 import { StatusCodes } from 'http-status-codes';
-import { createToken } from '../utils/jwt';
+import type IUser from '../interfaces/IUser';
+import UserModel from '../models/UserModel';
 import ApiErrors from '../utils/apiErrors';
+import { createToken } from '../utils/jwt';
 
 export default class UserService {
   private userModel: UserModel;
@@ -12,39 +12,47 @@ export default class UserService {
     this.userModel = new UserModel();
   }
 
-  public async create(user: IUser): Promise<string> {
-    const usernameExists = await this.userModel.find({
+  public async create(user: IUser): Promise<Partial<IUser>> {
+    const usernameExists = await this.userModel.findOne({
       username: user.username,
     });
 
-    if (usernameExists.length > 0)
+    if (usernameExists)
       throw new ApiErrors(
         'Username already registered.',
-        StatusCodes.FORBIDDEN
+        StatusCodes.FORBIDDEN,
       );
     const hashedPassword = await bcrypt.hash(user.password, 10);
 
     user.password = hashedPassword;
     const newUser = await this.userModel.create(user);
 
-    const token = createToken({ username: user.username });
-
-    return token;
+    return {
+      id: newUser.id,
+      username: newUser.username,
+    };
   }
 
-  //   public async login(user: IUser): Promise<string> {
-  //     const userLogin = await this.userModel.findByEmail(user.email);
-  //     if (!userLogin) throw new ApiErrors('Wrong email.', StatusCodes.FORBIDDEN);
+  public async login(user: IUser): Promise<string> {
+    const userLogin = await this.userModel.findOne({
+      username: user.username,
+    });
+    console.log(userLogin)
+    if (!userLogin)
+      throw new ApiErrors('Wrong username.', StatusCodes.FORBIDDEN);
 
-  //     const validPassword = await bcrypt.compare(
-  //       user.password,
-  //       userLogin.password
-  //     );
+    const validPassword = await bcrypt.compare(
+      user.password,
+      userLogin.password,
+    );
 
-  //     if (!validPassword)
-  //       throw new ApiErrors('Wrong password.', StatusCodes.FORBIDDEN);
+    if (!validPassword)
+      throw new ApiErrors('Wrong password.', StatusCodes.FORBIDDEN);
 
-  //     const token = createToken({ email: userLogin.email });
-  //     return token;
-  //   }
+    const token = createToken({
+      id: userLogin.id,
+      username: userLogin.username,
+    });
+    return token;
+  }
 }
