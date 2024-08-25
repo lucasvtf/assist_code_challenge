a0c361fc02bf62cbe3c638d88b04f6a6c6e276e4
Lucas Victor <lucassvtf@gmail.com>
2024-08-25 15:39:57 -0300

    feat: create the MessageService.

--- backend/src/services/MessageService.ts
+++ backend/src/services/MessageService.ts
@@ -1,3 +1,4 @@
+import type { Types } from 'mongoose';
 import type IMessage from '../interfaces/IMessageModel';
 import MessageModel from '../models/MessageModel';
 
@@ -10,5 +11,23 @@ export default class MessageService {
 
   public async create(message: IMessage): Promise<IMessage> {
     return this.messageModel.create(message);
+  }
+
+  public async getAllMessagesById(
+    userId: Types.ObjectId,
+    ourUserId: Types.ObjectId,
+  ): Promise<IMessage[]> {
+    const messages = await this.messageModel.find({
+      $or: [
+        { sender: userId, recipient: ourUserId },
+        { sender: ourUserId, recipient: userId },
+      ],
+    });
+
+    const sortedMessages = messages.sort((a, b) => {
+      return a.createdAt.getTime() - b.createdAt.getTime();
+    });
+
+    return sortedMessages;
   }
 }
